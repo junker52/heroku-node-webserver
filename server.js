daea00833815ccd38930613d9f0e9763df616c51
Ricard <r.molina91@gmail.com>
2018-08-20 12:50:28 +0200

    Using hbs partials to template web site

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs');
 
 //Servir pagina estatica
-app.use(express.static( __dirname+'/public'));
+app.use(express.static( __dirname +'/public'));
 
 //Express Engine
+hbs.registerPartials( __dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
+//Express Rendering Views
 app.get('/', function (req, res) {
 
-    //res.send(persona);
-    res.render('home',{name: 'Ricard'});
+    res.render('home',{name: 'Ricard', title: "Home Page"});
 })
 
- 
+app.get('/about', function (req, res) {
+
+    res.render('about',{title: "About Page"});
+})
+
+
+
+//Express sending JSON
 app.get('/hola', function (req, res) {
-    res.send('Hola Mundo');
+    let persona = {
+        nombre: 'Juan',
+        edad: 23,
+        url: req.url
+    };
+    res.send(persona);
 })
 
 app.listen(3000, () => {
